e6abea20043969994b405b0e0a46beccd19d6df8
androfrost <43192015+androfrost@users.noreply.github.com>
2022-02-01 08:40:36 -0500

    Added Try/Catch for I/O exceptions

--- src/fileprocessor/delimitedFileProcessor.java
+++ src/fileprocessor/delimitedFileProcessor.java
@@ -4,85 +4,92 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class delimitedFileProcessor {
 	public static ArrayList<ArrayList<String>> delimitedFileReader (String tcFileName, String delimChar, String charGroup)  throws Exception {
 		
-		File file = new File(tcFileName);			// Create file object based on given file name string
 		ArrayList<ArrayList<String>> processedInfo = new ArrayList<ArrayList<String>> ();
-		BufferedReader br = new BufferedReader(new FileReader(file));
 		
-		String st;
-		int lineNum = 0;
-		// Loop through lines of the given text file
-		while ((st = br.readLine()) != null) {
-
-			boolean isCharGroup = false;
-			boolean isProcessString = false;
-			String currentChar = "", previousChar = "";
-			String processedItem = "";
-			int processCount = -1;
-			int charCount = 0;
-			processedInfo.add(new ArrayList<String>());
+		try {
+			File file = new File(tcFileName);			// Create file object based on given file name string
 			
-			// Loop through all characters in the given file line
-			for (int charpos = 0; charpos < st.length(); charpos++) {
-				currentChar = Character.toString(st.charAt(charpos));
-				charCount = charCount + 1;
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String st;
+			int lineNum = 0;
+			// Loop through lines of the given text file
+			while ((st = br.readLine()) != null) {
+	
+				boolean isCharGroup = false;
+				boolean isProcessString = false;
+				String currentChar = "", previousChar = "";
+				String processedItem = "";
+				int processCount = -1;
+				int charCount = 0;
+				processedInfo.add(new ArrayList<String>());
 				
-				// Turn isCharGroup on and off each time it is hit to split what is processed and what is not
-				if ((currentChar.contentEquals(charGroup)) && (Boolean.compare(isProcessString, false) == 0)) {
-					if (Boolean.compare(isCharGroup, false) == 0) {
-						isCharGroup = true;
-						processCount = processCount + 1;
+				// Loop through all characters in the given file line
+				for (int charpos = 0; charpos < st.length(); charpos++) {
+					currentChar = Character.toString(st.charAt(charpos));
+					charCount = charCount + 1;
+					
+					// Turn isCharGroup on and off each time it is hit to split what is processed and what is not
+					if ((currentChar.contentEquals(charGroup)) && (Boolean.compare(isProcessString, false) == 0)) {
+						if (Boolean.compare(isCharGroup, false) == 0) {
+							isCharGroup = true;
+							processCount = processCount + 1;
+						}
+						else {
+							isCharGroup = false;
+						}
 					}
-					else {
-						isCharGroup = false;
-					}
-				}
-				
-				if (!(currentChar.contentEquals(charGroup)) && !(currentChar.contentEquals(delimChar)) && (Boolean.compare(isCharGroup, false) == 0)) {
-					if (Boolean.compare(isProcessString, false) == 0) {
-						isProcessString = true;
-						processCount = processCount + 1;
-					}
-					else {
+					
+					if (!(currentChar.contentEquals(charGroup)) && !(currentChar.contentEquals(delimChar)) && (Boolean.compare(isCharGroup, false) == 0)) {
+						if (Boolean.compare(isProcessString, false) == 0) {
+							isProcessString = true;
+							processCount = processCount + 1;
+						}
+						else {
+							if (currentChar.contentEquals(delimChar))
+								isProcessString = false;
+						}
+					} else {
 						if (currentChar.contentEquals(delimChar))
 							isProcessString = false;
 					}
-				} else {
-					if (currentChar.contentEquals(delimChar))
-						isProcessString = false;
+					
+					// When processing add each character to the string to be added to arraylist
+					if ((Boolean.compare(isCharGroup, true) == 0) && (!currentChar.contentEquals(charGroup))) {
+						processedItem = processedItem + currentChar;
+					}
+					
+					if (Boolean.compare(isProcessString, true) == 0){
+						processedItem = processedItem + currentChar;
+					}
+					
+					if (previousChar.contentEquals(",") && currentChar.contentEquals(",")) {
+						processCount = processCount + 1;
+						//processedItem = processedItem + " ";
+					}
+					
+					// Add processed string info to arraylist
+					if (((Boolean.compare(isCharGroup, false) == 0) && (!currentChar.contentEquals(charGroup)) && (currentChar.contentEquals(delimChar))) || (charCount == st.length())) {
+						processedInfo.get(lineNum).add(processCount, processedItem);
+						processedItem = "";
+					}
+					
+					//if (previousChar.contentEquals(",") && currentChar.contentEquals(",")) {
+					//	processedInfo.get(lineNum).add(processCount, " ");
+					//}
+					
+					previousChar = currentChar;
 				}
-				
-				// When processing add each character to the string to be added to arraylist
-				if ((Boolean.compare(isCharGroup, true) == 0) && (!currentChar.contentEquals(charGroup))) {
-					processedItem = processedItem + currentChar;
-				}
-				
-				if (Boolean.compare(isProcessString, true) == 0){
-					processedItem = processedItem + currentChar;
-				}
-				
-				if (previousChar.contentEquals(",") && currentChar.contentEquals(",")) {
-					processCount = processCount + 1;
-					//processedItem = processedItem + " ";
-				}
-				
-				// Add processed string info to arraylist
-				if (((Boolean.compare(isCharGroup, false) == 0) && (!currentChar.contentEquals(charGroup)) && (currentChar.contentEquals(delimChar))) || (charCount == st.length())) {
-					processedInfo.get(lineNum).add(processCount, processedItem);
-					processedItem = "";
-				}
-				
-				//if (previousChar.contentEquals(",") && currentChar.contentEquals(",")) {
-				//	processedInfo.get(lineNum).add(processCount, " ");
-				//}
-				
-				previousChar = currentChar;
+				lineNum = lineNum + 1;
 			}
-			lineNum = lineNum + 1;
+		} catch(IOException ioe) {
+			System.out.println("Delimited File Issues");
 		}
 		
 		return processedInfo;
